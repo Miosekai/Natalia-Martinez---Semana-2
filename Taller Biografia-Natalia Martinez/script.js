609f62baf241b8fcebc78931a26692e77b0c820a
Natalia Martinez <81645472+Miosekai@users.noreply.github.com>
2023-04-03 01:58:40 -0500

    Add files via upload

---
+++ Taller Biografia-Natalia Martinez/script.js
@@ -0,0 +1,29 @@
+let biografyButton = document.getElementById("item-biografy");
+let homeButton = document.getElementById("item-home");
+let spotifyButton = document.getElementById("item-spotify");
+let fentyButton = document.getElementById("item-fenty");
+let referenceButton = document.getElementById("item-reference");
+
+/* Elaborado por Natalia Martinez */
+
+biografyButton.onclick = ((e) => {
+    document.documentElement.scrollTop = 1250
+});
+
+homeButton.onclick = ((e) => {
+    document.documentElement.scrollTop = 0
+});
+
+spotifyButton.onclick = ((e) => {
+    document.documentElement.scrollTop = 2300
+});
+
+fentyButton.onclick = ((e) => {
+    document.documentElement.scrollTop = 3200
+});
+
+referenceButton.onclick = ((e) => {
+    document.documentElement.scrollTop = 4200
+});
+
+
